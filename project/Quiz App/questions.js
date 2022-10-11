720259356cf59c0cba8d2b5784322ac6be293a72
Dhrupad Chakraborty <91726340+dhrupad17@users.noreply.github.com>
2022-10-11 21:12:37 +0530

    I have added a Quiz App website .
    It is made using html css and javascript

---
+++ project/Quiz App/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who among the following first coined the term wiki?",
+    answer: "Ward Cunningham",
+    options: [
+      "Julian Assange",
+      "Rafael Correa",
+      "Ward Cunningham",
+      "Greg Chesson"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does the Internet prefix WWW stand for?",
+    answer: "World Wide Web",
+    options: [
+      "Wide Width Wickets",
+      "World Wide Web",
+      "Worldwide Weather",
+      "Western Washington World"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which one is the first search engine in Internet?",
+    answer: "Archie",
+    options: [
+      "Archie",
+      "Google",
+      "Altavista",
+      "WAIS"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which one is the first web browser invented in 1990?",
+    answer: "Nexus",
+    options: [
+      "Internet Explorer",
+      "Mosaic",
+      "Mozilla",
+      "Nexus"
+    ]
+  },
+  {
+    numb: 7,
+    question: "First Computer Virus is known as",
+    answer: "Creeper Virus",
+    options: [
+      "Rabbit",
+      "SCA Virus",
+      "Creeper Virus",
+      "Elk Cloner"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which one is the first fully supported 64-bit Operating System?",
+    answer: "Linux",
+    options: [
+      "Linux",
+      "Windows XP",
+      "Mac",
+      "Windows Vista"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Who is also known as Father of Super-Computing?",
+    answer: "Vijay Bhatkar",
+    options: [
+      "Jayant Narlikar",
+      "Raghunath patil",
+      "Nandan Nilekani",
+      "Vijay Bhatkar"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does URL stands for?",
+    answer: "Uniform Resource Locator",
+    options: [
+      "Universal Resource Locator",
+      "Uniform Resource Locator",
+      "Unique Resource Locator",
+      "Unsafe Resource Locator"
+    ]
+  }
+ 
+];
